73e1430887b05cf0c5819f1e563875ec9de4a5f4
faizaaulia <faizaaulia.id@gmail.com>
2021-03-11 20:23:30 +0700

    update & delete

--- controllers/products.js
+++ controllers/products.js
@@ -12,7 +12,7 @@ export const createProduct = (req, res) => {
   let data = { ...req.body, product_id: uuidv4() }
   let query = `INSERT INTO products SET ?`
   conn.query(query, data, (err, result) => {
-    err ? console.log(err) : res.send(result)
+    err ? console.log(err) : res.send(`Berhasil menambahkan produk ${data.product_name}`)
   })
 }
 
@@ -20,5 +20,20 @@ export const getProduct = (req, res) => {
   let query = `SELECT * FROM products where product_id = '${req.params.id}'`
   conn.query(query, (err, result) => {
     err ? console.log(err) : res.send(result)
+  })
+}
+
+export const deleteProduct = (req, res) => {
+  let query = `DELETE FROM products where product_id = '${req.params.id}'`
+  conn.query(query, (err, result) => {
+    err ? console.log(err) : res.send(`Berhasil menghapus produk ${req.params.id}`)
+  })
+}
+
+export const updateProduct = (req, res) => {
+  let data = req.body
+  let query = `UPDATE products SET ? WHERE product_id = '${req.params.id}'`
+  conn.query(query, data, (err, result) => {
+    err ? console.log(err) : res.send(`Berhasil mengubah produk`)
   })
 }
--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getProducts, createProduct, getProduct } from '../controllers/products.js'
+import { getProducts, createProduct, getProduct, deleteProduct, updateProduct } from '../controllers/products.js'
 
 const router = express.Router()
 
@@ -8,5 +8,9 @@ router.get('/', getProducts)
 router.post('/', createProduct)
 
 router.get('/:id', getProduct)
+
+router.delete('/:id', deleteProduct)
+
+router.put('/:id', updateProduct)
 
 export default router
